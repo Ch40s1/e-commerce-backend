b5346bf41d89493eee93a5bb3740e778899983f5
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-08-31 13:53:31 -0500

    made delete and put for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,12 +46,47 @@ router.post('/', (req, res) => {
   });
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (updatedCategory[0] === 0) {
+      res.status(404).json({ message: 'No category with that id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category updated successfully!' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedCategory) {
+      res.status(404).json({ message: 'No category with that id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category deleted successfully!' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
