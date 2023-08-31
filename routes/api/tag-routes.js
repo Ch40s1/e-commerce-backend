6efc3565943821b6e345d9fd7d4607fd0ef8eacf
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-08-31 15:17:27 -0500

    added post, put and delete for the tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,6 +5,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
+    // finds all tags and include the product according to the tag id
     const tagData = await Tag.findAll({
       include: [{
         model:Product,
@@ -20,12 +21,14 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try{
+    // gets all tags by the primary key
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{
         model:Product,
         through: ProductTag
       }]
     });
+    // if tags didn't work then the there is no tag with that id
     if(!tagData){
       res.status(404).json({message: "No Tag with that id"});
       return;
@@ -37,16 +40,52 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+//  It should look something like this
+//  {
+//     "tag_name": "test_tag"
+//  }
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(201).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (updatedTag[0] === 0) {
+      res.status(404).json({ message: 'No Tag with that id' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag updated successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedTag) {
+      res.status(404).json({ message: 'No Tag with that id' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag deleted successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
